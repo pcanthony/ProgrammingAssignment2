69ad3eae6b531fc07c47bd25920a4d140b819c2b
pcanthony <pcanthony@gmail.com>
2014-05-25 12:24:08 -0700

    Added line breaks to comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,8 @@
-## The following two functions together efficiently calculate the inverse of a square matrix.  When the inverse is requested for the first time, the value is calculated and cached.  When the inverse is requested subsequent times, and the matrix has not changed, the value is read from the cache.
+# The following two functions together efficiently calculate the
+# inverse of a square matrix.  When the inverse is requested for
+# the first time, the value is calculated and cached.  When the 
+# inverse is requested subsequent times, and the matrix has not 
+# changed, the value is read from the cache.
 
 
 # makeCacheMatrix creates a special "matrix", which is really a list containing a function to:
@@ -23,7 +27,12 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-# cacheSolve calculates the inverse of the matrix created with makeCacheMatrix. However, it first checks to see if the inverse has already been calculated. If so, it gets the inverse from the cache and skips the computation. Otherwise, it calculates the inverse of the matrix and sets the value of the inverse in the cache via the setinv function.
+# cacheSolve calculates the inverse of the matrix created with
+# makeCacheMatrix. However, it first checks to see if the inverse
+# has already been calculated. If so, it gets the inverse from the
+# cache and skips the computation. Otherwise, it calculates the
+# inverse of the matrix and sets the value of the inverse in the
+# cache via the setinv function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
